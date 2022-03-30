b17a0b1789b9f165778cf45cac98b5ab98147050
Felix Gnass <fgnass@gmail.com>
2022-03-30 16:35:11 +0200

    fix: wrap in async function

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+async function run() {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-const res = await octokit.actions.getRepoPublicKey();
-console.log("Get public key", res);
+  const res = await octokit.actions.getRepoPublicKey();
+  console.log("Get public key", res);
+}
+
+run();
