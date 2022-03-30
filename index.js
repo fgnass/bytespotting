4e176c4a8de48b1c30c3ab2bc41d84900a55ea35
Felix Gnass <fgnass@gmail.com>
2022-03-30 16:36:35 +0200

    fix: provide ower/repo

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const octokit = new Octokit();
 
 async function run() {
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-
-  const res = await octokit.actions.getRepoPublicKey();
+  const res = await octokit.actions.getRepoPublicKey({ owner, repo });
   console.log("Get public key", res);
 }
 
