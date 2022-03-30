6f0eb348558a3af75a5919e5052a6809851c4f8c
Felix Gnass <fgnass@gmail.com>
2022-03-30 17:16:57 +0200

    ci: cache deps

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const octokit = new Octokit({ auth: process.env.PAT });
 
 async function run() {
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-  const res = await octokit.rest.actions.getRepoPublicKey({ owner, repo });
-  console.log("Get public key", res);
+  const {
+    data: { key },
+  } = await octokit.rest.actions.getRepoPublicKey({ owner, repo });
+  console.log("Get public key", key);
 }
 
 run();
