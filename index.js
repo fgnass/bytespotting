e717c0a44086a6b75401ad9ddcb98b281a4ce9de
Felix Gnass <fgnass@gmail.com>
2022-03-30 16:33:09 +0200

    ci: try to get public key

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("Environment");
-console.log(process.env);
+const { Octokit } = require("@octokit/action");
+const octokit = new Octokit();
+
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+const res = await octokit.actions.getRepoPublicKey();
+console.log("Get public key", res);
